b0c71975328f307a1579b21a5e8215918488f8a0
Nidhi Patel <nidhi2007patel@gmail.com>
2021-10-15 16:23:46 +0530

    Files have been committed

---
+++ OnlineshopingCart/DBConnection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace OnlineshopingCart
+{
+    public class DBConnection
+    {
+        public static void OpenConnection(SqlConnection con)
+        {
+            try
+            {
+                if (con.State == ConnectionState.Open)
+                {
+                    con.Close();
+                }
+                con.ConnectionString = @"Data Source=(LocalDb)\MSSQLLocalDB;Integrated Security=True";
+                con.Open();
+            }
+            catch (Exception e)
+            {
+                
+            }
+        }
+        public static void CloseConnection(SqlConnection con)
+        {
+            try
+            {
+                con.Close();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
---
+++ OnlineshopingCart/ProductDetails.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OnlineshopingCart
+{
+    public partial class ProductDetails : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int pid = int.Parse(Request.QueryString["pid"]);
+
+            SqlConnection con = new SqlConnection();
+
+            DBConnection.OpenConnection(con);
+
+            SqlCommand cmd = new SqlCommand("select * from Products where ProductID=" + pid, con);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                lblProductID.Text = pid.ToString();
+                lblProductName.Text = dr[1].ToString();
+                lblProductPrice.Text = dr[2].ToString();
+                Image1.ImageUrl = dr[3].ToString();
+                lblAmount.Text = CalculateAmount(txtQuantity.Text, dr[2].ToString());
+            }
+
+            dr.Close();
+
+            cmd.Dispose();
+
+            DBConnection.CloseConnection(con);
+        }
+        private string CalculateAmount(string Quantity, string Price)
+        {
+            int qty = int.Parse(Quantity);
+
+            Decimal price = Decimal.Parse(Price);
+
+            Decimal amount = qty * price;
+
+            return amount.ToString();
+        }
+
+        protected void txtQuantity_TextChanged(object sender, EventArgs e)
+        {
+            lblAmount.Text = CalculateAmount(txtQuantity.Text, lblProductPrice.Text);
+        }
+
+        protected void btnAddToCart_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection();
+
+            DBConnection.OpenConnection(con);
+
+            string query = "insert into Cart (ProductID, Quantity, Price, Amount, CartID) values (" + lblProductID.Text + ", " + txtQuantity.Text + ", " + lblProductPrice.Text + ", " + lblAmount.Text + ", '" + Session.SessionID + "')";
+
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            cmd.ExecuteNonQuery();
+
+            Session["CartID"] = Session.SessionID;
+
+            cmd.Dispose();
+
+            DBConnection.CloseConnection(con);
+
+            ClientScript.RegisterStartupScript(GetType(), "Popup", "successalert();", true);
+        }
+    }
+}
